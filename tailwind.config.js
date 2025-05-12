f8514c89a21c63e4ab10f5c63f2228df688077a1
shoebabedin <shoeb.abedin@intelli.global>
2025-05-12 17:08:03 +0600

    update

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+ /** @type {import('tailwindcss').Config} */
+ const defaultTheme = require("tailwindcss/defaultTheme");
+
+ export default {
+  content: ["*", "./pages/*html", "./src/**/*.{js,jsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "16px",
+      },
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1240px",
+        "2xl": "1352px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        "Playfair-Displa": ["Playfair Displa", ...defaultTheme.fontFamily.sans],
+        "Inter": ["Inter", "sans-serif"],
+      },
+      colors: {
+        primary: '#D5A342',
+        secondary: '#14283C'
+      },
+    },
+  },
+  plugins: [],
+}
