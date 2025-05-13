e4a77eae0ed7db28dbb7ce9101b16e92376d6f14
shoebabedin <shoeb.abedin@intelli.global>
2025-05-13 19:01:36 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  /*======================= 09: Testimonial slider =========================*/
+  /*======================= hero-slider =========================*/
   var swiper = new Swiper(".hero-slider", {
     loop: true,
     pagination: {
@@ -12,7 +12,21 @@ $(document).ready(function () {
       }
     }
   });
+  /*======================= card-slider =========================*/
+  var swiper = new Swiper(".card-slider", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        const slides = document.querySelectorAll('.swiper-slide');
+        const title = slides[index]?.dataset?.title || `Slide ${index + 1}`;
+        return `<span class="${className}" data-tooltip="${title}"></span>`;
+      }
+    }
+  });
 
+   /*======================= grab-scroll =========================*/
   const slider = document.querySelector('.grab-scroll');
   let isDown = false;
   let startX;
